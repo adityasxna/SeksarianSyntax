5dcb753218da2819903a802c7cf63742bd9235c4
NIDHI AGRAWAL <136151888+nidhi500@users.noreply.github.com>
2024-06-15 07:31:47 +0530

    backup
    jmm

--- client/src/components/FlexBetween.tsx
+++ client/src/components/FlexBetween.tsx
@@ -2,10 +2,10 @@ import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
 
-from FlexBetween = styled(Box)({
+From FlexBetween = styled(Box)({
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center"
 });
 
-export degault Nac
+export default Nav;
